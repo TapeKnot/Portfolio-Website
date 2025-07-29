3023b3baa45d89b20784859888a4b6d423d2a120
Benjamin Levy <benelevy@icloud.com>
2025-07-29 17:23:15 -0400

    More ESLint Fixes

--- src/components/slideshow/slideshow.tsx
+++ src/components/slideshow/slideshow.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import Image from "next/image";
 import "./slideshow.css";
 
-export const Slideshow = ({data}) => {
+export const Slideshow = ({data}: any) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const nextSlide = () => {
@@ -19,9 +19,9 @@ export const Slideshow = ({data}) => {
             <div className="slideshow-container">
                 <button className="slide-arrow-left" onClick={prevSlide}>{"<--"}</button>
                 <div className="slide-container">
-                    { data.map((item, index) => {
+                    { data.map((item: any, index: any) => {
                         if (item.src.includes("https://www.youtube.com/embed/")) {
-                            return <iframe className={currentIndex === index ? "slide-active" : "slide"} src={item.src} alt={item.alt} key={index} fill="true"/>
+                            return <iframe className={currentIndex === index ? "slide-active" : "slide"} src={item.src} key={index}/>
                         }
                         else {
                             return <Image className={currentIndex === index ? "slide-active" : "slide"} src={item.src} alt={item.alt} key={index} fill={true}/>
@@ -31,7 +31,7 @@ export const Slideshow = ({data}) => {
                 <button className="slide-arrow-right" onClick={nextSlide}>{"-->"}</button>
             </div>
             <span className="slide-indicator-container">
-                { data.map((item, index) => {
+                { data.map((item: any, index: any) => {
                     return <button className={currentIndex === index ? "slide-indicator-active" : "slide-indicator"} key={index} onClick={() => setCurrentIndex(index)}></button>
                 })}
             </span>
