a0091240bb8968811ebbdffe0a643cac52bd4040
Benjamin Levy <benelevy@icloud.com>
2025-07-29 16:25:55 -0400

    Header Nav Fix, Changed Email Social Link to Resume Link

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
       <p className="text-align-center">Website made by Benjamin Levy</p>
       <br/>
       <div className="social-link-container">
-        <a href="mailto:benjaminlevy.bel@gmail.com" target="_blank"><Image className="social-link" src='/social-icons/email-logo.png' alt="email" width={50} height={50}/></a>
+        <a href="/Benjamin-Levy-Resume.pdf" target="_blank"><Image className="social-link" src='/social-icons/resume-logo.png' alt="resume" width={50} height={50}/></a>
         <a href="https://www.linkedin.com/in/levybe/" target="_blank"><Image className="social-link" src='/social-icons/linkedin-logo.png' alt="linkedin" width={50} height={50}/></a>
         <a href="https://github.com/TapeKnot" target="_blank"><Image className="social-link" src='/social-icons/github-logo.png' alt="github" width={50} height={50}/></a>
       </div>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,13 +4,15 @@ import './header.css'
 export default function Header() {
   return (
     <header>
-      <h2 className="text-italic">Benjamin Levy</h2>
-      <div className="h-right-grid-section">
-        <Link className="text-bold" href="/">Home</Link>
-        <Link className="text-bold" href="/projects">Projects</Link>
-        <Link className="text-bold" href="/about">About</Link>
-        <a className="text-bold" href="/Benjamin-Levy-Resume.pdf" target="_blank">Resume</a>
-      </div>
+      <nav>
+        <h2 className="text-italic">Benjamin Levy</h2>
+        <div className="h-right-grid-section">
+          <Link className="text-bold" href="/">Home</Link>
+          <Link className="text-bold" href="/projects">Projects</Link>
+          <Link className="text-bold" href="/about">About</Link>
+          <a className="text-bold" href="/Benjamin-Levy-Resume.pdf" target="_blank">Resume</a>
+        </div>
+      </nav>
     </header>
   );
 }
