bdcba369d7a6b7a542cc4bed9961db2645795c30
Benjamin Levy <benelevy@icloud.com>
2025-07-29 11:54:45 -0400

    Contact Form Fixes

--- src/components/contact-form/contact-form.tsx
+++ src/components/contact-form/contact-form.tsx
@@ -1,6 +1,7 @@
 "use client"
 import './contact-form.css'
-import React, { FormEvent, useState } from 'react'
+import React, { useEffect, FormEvent, useState } from 'react'
+import { usePathname } from 'next/navigation'
 
 export const ContactForm = () => {
     const [name, setName] = useState('')
@@ -8,6 +9,15 @@ export const ContactForm = () => {
     const [subject, setSubject] = useState('')
     const [message, setMessage] = useState('')
     const [submitted, setSubmitted] = useState(false);
+
+    const pathname = usePathname();
+    useEffect(() => {   // Reset form state on page change
+        setName('');
+        setEmail('');
+        setSubject('');
+        setMessage('');
+        setSubmitted(false);
+    }, [pathname]);
 
     const onSubmit = async (e: FormEvent) => {
         e.preventDefault();
@@ -26,13 +36,6 @@ export const ContactForm = () => {
             const res = await fetch(request);
             const body = await res.json();
             setSubmitted(body.submitted);
-            
-            if (submitted) {
-                setName("");
-                setEmail("");
-                setSubject("");
-                setMessage("");
-            }
         }
         catch(err: any) {
             console.error('Error', err)
@@ -40,58 +43,67 @@ export const ContactForm = () => {
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <h2 className="text-align-center">Contact Me</h2>
-            <hr/>
-            <p>Want to get in touch? Send me a message here!</p>
-            <div className="input-box">
-                <label>Full Name</label><br/>
-                <input 
-                    value={name} 
-                    className="field" 
-                    type="text" 
-                    name="name" 
-                    placeholder="Name" 
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                />
-            </div>
-            <div className="input-box">
-                <label>Email Address</label><br/>
-                <input 
-                    value={email}
-                    className="field" 
-                    type="email" 
-                    name="email" 
-                    placeholder="Email" 
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-            </div>
-            <div className="input-box">
-                <label>Subject</label><br/>
-                <input 
-                    value={subject}
-                    className="field" 
-                    type="text" 
-                    name="subject" 
-                    placeholder="Subject" 
-                    onChange={(e) => setSubject(e.target.value)}
-                    required
-                />
-            </div>
-            <div className="input-box">
-                <label>Message</label><br/>
-                <textarea 
-                    value={message}
-                    className="field message" 
-                    name="message" 
-                    placeholder="Start typing here..." 
-                    onChange={(e) => setMessage(e.target.value)}
-                    required
-                />
-            </div>
-            <button type="submit">Send</button>
-        </form>
+        <div>
+            {submitted
+                ? <div className="foreground-section">
+                    <h3 className="text-align-center">Message sent! I will get back to you as soon as I can.</h3>
+                </div>
+                : <form onSubmit={onSubmit}>
+                    <h2 className="text-align-center">Contact Me</h2>
+                    <hr/>
+                    <p>Want to get in touch? Send me a message here!</p>
+                    <div className="input-box">
+                        <label>Full Name</label><br/>
+                        <input 
+                            value={name} 
+                            className="field" 
+                            type="text" 
+                            name="name" 
+                            placeholder="Name" 
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-box">
+                        <label>Email Address</label><br/>
+                        <input 
+                            value={email}
+                            className="field" 
+                            type="email" 
+                            name="email" 
+                            placeholder="Email" 
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-box">
+                        <label>Subject</label><br/>
+                        <input 
+                            value={subject}
+                            className="field" 
+                            type="text" 
+                            name="subject" 
+                            placeholder="Subject" 
+                            onChange={(e) => setSubject(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-box">
+                        <label>Message</label><br/>
+                        <textarea 
+                            value={message}
+                            className="field message" 
+                            name="message" 
+                            placeholder="Start typing here..." 
+                            onChange={(e) => setMessage(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="flex-div justify-content-center">
+                        <button type="submit">Send</button>
+                    </div>
+                </form>
+            }
+        </div>
     )
 }
