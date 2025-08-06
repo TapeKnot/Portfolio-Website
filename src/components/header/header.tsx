892fcbeacd64becb9149961aa3676d2eb0ecd63f
Benjamin Levy <benelevy@icloud.com>
2025-08-06 16:35:35 -0400

    Hamburger Nav Menu Implemented

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,22 @@
 import Link from 'next/link'
 import './header.css'
+import { HamburgerMenu } from "../hamburger-menu/hamburger-menu"
 
 export default function Header() {
   return (
     <header>
-      <nav>
-        <h2 className="text-italic">Benjamin Levy</h2>
-        <div className="h-right-grid-section">
-          <Link className="text-bold" href="/">Home</Link>
-          <Link className="text-bold" href="/projects">Projects</Link>
-          <Link className="text-bold" href="/about">About</Link>
-          <a className="text-bold" href="/Benjamin-Levy-Resume.pdf" target="_blank">Resume</a>
-        </div>
-      </nav>
+      <HamburgerMenu/>
+      <div className="header-section">
+        <nav>
+          <h2 className="text-italic">Benjamin Levy</h2>
+          <div className="h-right-grid-section">
+            <Link className="text-bold" href="/">Home</Link>
+            <Link className="text-bold" href="/projects">Projects</Link>
+            <Link className="text-bold" href="/about">About</Link>
+            <a className="text-bold" href="/Benjamin-Levy-Resume.pdf" target="_blank">Resume</a>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 }
