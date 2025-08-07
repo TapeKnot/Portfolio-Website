a2397ed7bc736f07a440c770d19b70d0f4b33144
Benjamin Levy <benelevy@icloud.com>
2025-08-06 21:27:59 -0400

    Merged Hamburger Menu With Header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,22 +1,53 @@
+"use client"
 import Link from 'next/link'
 import './header.css'
-import { HamburgerMenu } from "../hamburger-menu/hamburger-menu"
+import React, { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation'
 
-export default function Header() {
-  return (
+export const Header = () => {
+  const [opened, setOpened] = useState(false);
+
+  const handleClick = () => {
+    setOpened(!opened);
+  };
+
+  const pathname = usePathname();
+  useEffect(() => {   // Reset state on page change
+      setOpened(false);
+  }, [pathname]);
+
+  return(
     <header>
-      <HamburgerMenu/>
-      <div className="header-section">
-        <nav>
+      <nav>
+        <div className="nav-bar">
           <h2 className="text-italic">Benjamin Levy</h2>
-          <div className="h-right-grid-section">
+          <button className="hamburger-menu" onClick={handleClick}>
+              {opened
+                ? "X"
+                : <div className="hamburger-menu-icon">
+                  <span/>
+                  <span/>
+                  <span/>
+                </div>
+              }
+          </button>
+          <div className="nav-button-container">
             <Link className="text-bold" href="/">Home</Link>
             <Link className="text-bold" href="/projects">Projects</Link>
             <Link className="text-bold" href="/about">About</Link>
             <a className="text-bold" href="/Benjamin-Levy-Resume.pdf" target="_blank">Resume</a>
           </div>
-        </nav>
-      </div>
+        </div>
+        {opened
+          ? <div className="mobile-nav-button-container">
+              <Link className="mobile-nav-button" href="/">Home</Link>
+              <Link className="mobile-nav-button" href="/projects">Projects</Link>
+              <Link className="mobile-nav-button" href="/about">About</Link>
+              <a className="mobile-nav-button" href="/Benjamin-Levy-Resume.pdf" target="_blank">Resume</a>
+          </div>
+          : <div/>
+        }
+      </nav>
     </header>
-  );
+  )
 }
